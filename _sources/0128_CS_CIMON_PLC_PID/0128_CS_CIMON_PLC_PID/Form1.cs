4fc5fb88fe2ad9ad9bcfc9f7a89f520b839eb30a
lrelu <newlik@naver.com>
2019-02-15 17:22:48 +0900

    : feat - 모드버스 통신, POWER LINK 통신, CCLINK 통신

---
+++ _sources/0128_CS_CIMON_PLC_PID/0128_CS_CIMON_PLC_PID/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+using stdole;
+
+
+namespace _0128_CS_CIMON_PLC_PID
+{
+	public partial class Form1 : Form
+	{
+		public object _cimon;
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
---
+++ _sources/0128_CS_CIMON_PLC_PID/VB_Dump/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Cimon As Object
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+End Class
